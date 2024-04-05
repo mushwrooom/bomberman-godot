907277a8f2714a8143a01ebe84b08b1dc927c4c1
Munkh-Erdene Enkhtsetseg <h0pqir@inf.elte.hu>
2024-04-05 19:55:41 +0000

    UI connected

--- UserInterface/UserInterface.cs
+++ UserInterface/UserInterface.cs
@@ -3,19 +3,13 @@ using System;
 
 public partial class UserInterface : Control
 {
-	// Called when the node enters the scene tree for the first time.
-	public override void _Ready()
-	{
-	}
-
-	// Called every frame. 'delta' is the elapsed time since the previous frame.
-	public override void _Process(double delta)
-	{
-	}
-
-	public void _on_play_pressed(){
-		GetTree().ChangeSceneToFile("res://main.tscn");
 	
+	private Main main;
+	public override void _Ready(){
+		main = GetNode<Main>("/root/Main");
+	}
+	public void _on_play_pressed(){
+		main.StartGame();	
 	}
 	public void _on_settings_pressed(){
 
--- UserInterface/mapUI.cs
+++ UserInterface/mapUI.cs
@@ -9,10 +9,6 @@ using System.Collections.Generic;
 
 public partial class mapUI : Control
 {
-
-	//private Main _main; // reeference to main
-
-
 	private Main main;
 
 	/// <summary>
@@ -26,81 +22,25 @@ public partial class mapUI : Control
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
-		// GetNode("Map1").Connect("pressed", this, nameof(_on_Map_selected), new Godot.Collections.Array { 1 });
-        // GetNode("Map2").Connect("pressed", this, nameof(_on_Map_selected), new Godot.Collections.Array { 2 });
-        // GetNode("Map3").Connect("pressed", this, nameof(_on_Map_selected), new Godot.Collections.Array { 3 });
-		// ConnectButtonSignal("Map1", 1);
-    	// ConnectButtonSignal("Map2", 2);
-    	// ConnectButtonSignal("Map3", 3);
-
-
-		// Node a = GD.Load("path_for_3maps");
-		mapButtonToScenePath.Add("Map1","res:pathtomap1");
-		mapButtonToScenePath.Add("Map2","res:pathtomap2");
-		mapButtonToScenePath.Add("Map3","res:pathtomap3");
-
-
 		main = GetNode<Main>("/root/Main");
-
-
-		foreach(var mapButton in mapButtonToScenePath.Keys){
-			ConnectButtonSignal(mapButton);
-		}
-		
-
-
+		mapButtonToScenePath.Add("Map1","res://scenes/maps/map.tscn");
+		mapButtonToScenePath.Add("Map2","res://scenes/maps/map2.tscn");
+		mapButtonToScenePath.Add("Map3","res://scenes/maps/map3.tscn");
 	}
 
-	/// <summary>
-    /// Connects a button signal for map selection.
-    /// </summary>
-    /// <param name="buttonName">The name of the button to connect.</param>
-
-	private void ConnectButtonSignal(string buttonName)
-	{
-    	var button = GetNodeOrNull<Button>(buttonName);
-        if (button != null)
-        {
-            //button.Connect("pressed", this, nameof(_on_Map_selected), new Godot.Collections.Array { buttonName });
-        }
-        else
-        {
-            GD.PrintErr($"Button {buttonName} not found.");
-        }
+	public void _on_map_1_pressed(){
+		main.currentMap = mapButtonToScenePath["Map1"];
 	}
-
-
-	 // This method is called when a map selection button is pressed.
-
-
-	 /// <summary>
-    /// Handles the event when a map selection button is pressed. Changes the current map in the game.
-    /// </summary>
-    /// <param name="buttonName">The name of the button that was pressed.</param>
-
-     private void _on_Map_selected(string buttonName)
-    {
-        if (mapButtonToScenePath.TryGetValue(buttonName, out var scenePath))
-        {
-            // Use the scene path to change the map in the Main class
-            Main.currentMap = scenePath;
-        }
-        else
-        {
-            GD.PrintErr($"Scene path for button {buttonName} not found.");
-        }
-    }
-
-	// Called every frame. 'delta' is the elapsed time since the previous frame.
-	public override void _Process(double delta)
-	{
+	public void _on_map_2_pressed(){
+		main.currentMap = mapButtonToScenePath["Map2"];
 	}
-
+	public void _on_map_3_pressed(){
+		main.currentMap = mapButtonToScenePath["Map3"];
+	}
 
 	/// <summary>
     /// Handles the event when the customize control button is pressed. Changes the scene to the player customization interface.
     /// </summary>
-
 	public void _on_customize_control_pressed(){
 		GetTree().ChangeSceneToFile("res://UserInterface/playerCustomize.tscn");
 	
